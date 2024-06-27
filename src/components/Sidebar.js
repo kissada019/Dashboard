212a54eb2dd8f12740d28d47e4f2e8dcd1e27755
LDAPHQ\Kissada.Lah <kissada.lah>
2024-06-27 11:15:28 +0700

    git check

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -41,7 +41,7 @@ function Sidebar({ children }) {
     ];
     return (
         <div className='container'>
-            <div style={{ width: isOpen ? "200px" : "50px" }} className='sidebar'>
+            <div style={{ width: isOpen ? "300px" : "50px" }} className='sidebar'>
                 <div className='top_section'>
                     <h1 style={{ display: isOpen ? "block" : "none" }} className='logo'>Logo</h1>
                     <div style={{ marginLeft: isOpen ? "50px" : "00px" }} className='bars'>
